8da248ab0cb04192e5ad78146d1779d530cb7242
yagosamu <166074053+yagosamu@users.noreply.github.com>
2026-04-09 10:08:40 -0300

    bug fixes and password reset flow

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('cadastro/', views.cadastro, name='cadastro'),
     path("login/", views.login, name='login'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path("dashboard/", views.dashboard, name='dashboard'),
     path("clientes/", views.clientes, name='clientes'),
     path("cliente/<int:id>", views.cliente, name='cliente'),
